a2f38a9cc0c6e2b7b4bc59e3a43a7a3189cf88bc
jpablomaure <jpablomaure@gmail.com>
2022-03-08 00:18:57 -0300

    corregido, ahora es un ciclo

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-let anoAnalizar = Number(prompt('Ingresa un año para saber si es bisiesto'));
-let div4 = anoAnalizar %4;
-let div100 = anoAnalizar %100;
-let div400 = anoAnalizar %400;
+let anoAnalizar = Number(prompt('Ingresa un año para saber si es bisiesto o ingrese 999 para terminar'));
+// let valor = String(anoAnalizar);
+let div4 = anoAnalizar % 4;
+let div100 = anoAnalizar % 100;
+let div400 = anoAnalizar % 400;
 
-
-if (((div4 == 0) && (div100 != 0)) || (div400 == 0)) {
-    alert(`${anoAnalizar} Es año bisiesto`);
-} else {
-    alert(`${anoAnalizar} No es año bisiesto`);
+while (anoAnalizar != 999) {
+    if (((div4 == 0) && (div100 != 0)) || (div400 == 0)) {
+        alert(`${anoAnalizar} Es año bisiesto`);
+    } else {
+        alert(`${anoAnalizar} No es año bisiesto`);
+    }
+    anoAnalizar = Number(prompt('Ingresa otro año o 999 para terminar'));
+    div4 = anoAnalizar % 4;
+    div100 = anoAnalizar % 100;
+    div400 = anoAnalizar % 400;
 }
+alert("Desafio terminado");
